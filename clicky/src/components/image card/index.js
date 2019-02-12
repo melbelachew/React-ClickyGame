2849990e0ff9271b8fc34aa32981f76705a8226e
Mel <melbelachew@gmail.com>
2019-02-12 11:57:48 -0500

    update

---
+++ clicky/src/components/image card/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ImageCard(props) {
+    return ( 
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap?"/>
+        <div class="card-body">
+          <h5 class="card-title">Image to click on</h5>
+        </div>
+    </div>    );
+}  
+
+export default ImageCard;
